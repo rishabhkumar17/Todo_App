04899efc5152b65a1fda547ca94366bce9de5015
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2022-09-25 03:30:44 +0530

    App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import TextField from '@mui/material/TextField'
+import { useEffect, useState } from 'react'
+import { Button } from '@mui/material'
+import { db } from './firebase_config'
+import firebase from 'firebase/compat/app'
+import TodoListItem from './todo'
+import { Margin } from '@mui/icons-material'
 
 function App() {
+  const [todos, setTodos] = useState([])
+  const [todoInput, setTodoInput] = useState('')
+
+  useEffect(() => {
+    getTodos()
+  }, []) // blank to run only on first launch
+
+  const getTodos = () => {
+    db.collection('todos').onSnapshot((querySnapshot) => {
+      setTodos(
+        querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          todo: doc.data().todo,
+          inprogress: doc.data().inprogress,
+        }))
+      )
+    })
+  }
+  const addTodo = (e) => {
+    e.preventDefault()
+    db.collection('todos').add({
+      inprogress: true,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      todo: todoInput,
+    })
+    setTodoInput('')
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Rishabh's Todo App</h1>
+      <div className="container">
+        <div>
+          {todos.map((todo, index) => (
+            <TodoListItem
+              todo={todo.todo}
+              inprogress={todo.inprogress}
+              id={todo.id}
+              index={index}
+              List={todos}
+            />
+          ))}
+          <form className="textBox">
+            <TextField
+              fullWidth
+              style={{ backgroundColor: '#eef2ff' }}
+              sx={{ mr: 2 }}
+              size="small"
+              id="fullWidth"
+              variant="outlined"
+              value={todoInput}
+              onChange={(e) => setTodoInput(e.target.value)}
+            />
+            <Button
+              style={{ backgroundColor: '#4711de' }}
+              size="small"
+              type="submit"
+              variant="contained"
+              onClick={addTodo}
+            >
+              Add
+            </Button>
+          </form>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
